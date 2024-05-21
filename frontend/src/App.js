bd8cbf8973ea42334a13a946fadfba0d4fc41d4d
szludora <szlucska.dora@gmail.com>
2024-05-21 14:14:27 +0200

    alap

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import Container from "react-bootstrap/Container";
+
+function App() {
+  return (
+    <Container className="App">
+      <HomePage/>
+    </Container>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,9 @@
+import Container from "react-bootstrap/Container";
+
+export default function HomePage() {
+  return (
+    <Container>
+      <p>This is homepage</p>
+    </Container>
+  );
+}
